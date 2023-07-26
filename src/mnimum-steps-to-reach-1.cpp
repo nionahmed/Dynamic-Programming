cc9c6aa521fcd6e7be987a7ab06b6d34e25c710c
Md Nurul Amin <nurulaminnion@gmail@gmail.com>
2023-07-26 10:56:01 +0600

    .

---
+++ src/mnimum-steps-to-reach-1.cpp
@@ -0,0 +1,53 @@
+
+///Find minimum number of steps to reach 1. each steps can defined as:
+/// 1. subtract 1 : n-=1;
+/// 2. divide by 2 : n/=2 if n%2==0;
+/// 3. divide by 3 : n/=3 if n%3==0;
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll N=200005;
+
+ll dp[N];
+ll mini_step(ll n){
+    if(n==1)return dp[n]=0;     // n[1] must equal to zero.
+    if(dp[n]!=0)return dp[n];
+
+    dp[n]= mini_step(n-1)+1;
+    if(n%2==0){
+        dp[n]=min(dp[n], mini_step(n/2)+1);
+    }
+    if(n%3==0){
+        dp[n]=min(dp[n], mini_step(n/3)+1);
+    }
+    return dp[n];
+
+}
+
+
+
+void solve()
+{
+    ll n; cin>>n;
+    //memset(dp, -1, sizeof dp);
+    cout<<mini_step(n)<<endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll t=1;
+    cin>>t;
+    for(ll i=1; i<=t; i++)
+    {
+        //cout<<"Case "<<i<<":"<<endl;
+        solve();
+    }
+    return 0;
+}
+
---
+++ src/number-of-ways-to reach-1.cpp
@@ -0,0 +1,53 @@
+
+///Find number of ways to reach 1. each steps can defined as:
+/// 1. subtract 1 : n-=1;
+/// 2. divide by 2 : n/=2 if n%2==0;
+/// 3. divide by 3 : n/=3 if n%3==0;
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll N=200005;
+
+ll dp[N];
+ll numberOFways(ll n){
+    if(n==1)return dp[n]=0;
+    if(n==2)return dp[n]=2;
+    if(n==3)return dp[n]=3;
+    if(dp[n]!=0)return dp[n];
+
+    dp[n]= numberOFways(n-1);
+    if(n%2==0){
+        dp[n]+=numberOFways(n/2);
+    }
+    if(n%3==0){
+        dp[n]+=numberOFways(n/3);
+    }
+    return dp[n];
+
+}
+
+
+
+void solve()
+{
+    ll n; cin>>n;
+    //memset(dp, -1, sizeof dp);
+    cout<<numberOFways(n)<<endl;
+
+}
+
+int main()
+{
+    ll t=1;
+    cin>>t;
+    for(ll i=1; i<=t; i++)
+    {
+        //cout<<"Case "<<i<<":"<<endl;
+        solve();
+    }
+    return 0;
+}
+
